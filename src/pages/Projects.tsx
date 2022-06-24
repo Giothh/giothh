bd879912479ca7f5c7bb70931a2b6feab3771264
Giothh <thammer_giovanni@outlook.com>
2022-06-24 01:22:24 +0100

    new change on Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import '../styles/Projects.css'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import coverImage from '../images/covers/animelon.png'
->>>>>>> parent of ba245c7 (Updates)
-=======
-import coverImage from '../images/covers/animelon.png'
->>>>>>> parent of ba245c7 (Updates)
 import { Link } from "react-router-dom";
 import { projectsItem } from '../services/fakeApi'
 
