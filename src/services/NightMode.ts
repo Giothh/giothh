6d52cf7b5aadcb42ca0cac02e02ce1156b9bd21e
Giothh <thammer_giovanni@outlook.com>
2022-06-25 01:32:02 +0100

    config try

--- src/services/NightMode.ts
+++ src/services/NightMode.ts
@@ -3,24 +3,24 @@ import { useCookies } from "react-cookie";
 export default function SetVariables() {
 	const [cookies, setCookies] = useCookies(['night-mode']);
 
-	// var iconsColor: string,
-	// 	fontColor: string,
-	// 	mainPageBackgroundColor: string,
-	// 	buttonBackGroundColor: string,
-	// 	navBarBackGroundColor: string,
-	// 	userProfilePhotoBoxShadow: string,
-	// 	innerImageBlockProjectPage: string,
-	// 	defaultBoxShadow: string
+	var iconsColor: string,
+		fontColor: string,
+		mainPageBackgroundColor: string,
+		buttonBackGroundColor: string,
+		navBarBackGroundColor: string,
+		userProfilePhotoBoxShadow: string,
+		innerImageBlockProjectPage: string,
+		defaultBoxShadow: string
 
-	if (cookies["night-mode"] === 'true') {
-		var userProfilePhotoBoxShadow = "0px 0px 35px 1px rgba(204, 219, 222, 0.64)"
-		var mainPageBackgroundColor = "#2D2D2D"
-		var buttonBackGroundColor = "#111111"
-		var navBarBackGroundColor = "#111111"
-		var iconsColor = "#ffffff"
-		var fontColor = "#ffffff"
-		var innerImageBlockProjectPage = "#AFAFAF"
-		var defaultBoxShadow = "4px 4px 7px 1px rgba(0, 0, 0, 0.55)"
+	if (cookies["night-mode"] === 'true' || cookies["night-mode"] === true ) {
+		userProfilePhotoBoxShadow = "0px 0px 35px 1px rgba(204, 219, 222, 0.64)"
+		mainPageBackgroundColor = "#2D2D2D"
+		buttonBackGroundColor = "#111111"
+		navBarBackGroundColor = "#111111"
+		iconsColor = "#ffffff"
+		fontColor = "#ffffff"
+		innerImageBlockProjectPage = "#AFAFAF"
+		defaultBoxShadow = "4px 4px 7px 1px rgba(0, 0, 0, 0.55)"
 
 		var newNightMode = {
 			iconsColor,
@@ -34,15 +34,15 @@ export default function SetVariables() {
 		}
 		return newNightMode 
 
-	} else if (cookies["night-mode"] === 'false'){
-		var userProfilePhotoBoxShadow = "0px 0px 35px 1px rgba(29, 30, 31, 0.87)"
-		var mainPageBackgroundColor = "#D1D1D1"
-		var buttonBackGroundColor = "#DFDFDF"
-		var navBarBackGroundColor = "#EDEDED"
-		var iconsColor = "#000000"
-		var fontColor = "#000000"
-		var innerImageBlockProjectPage = "#AFAFAF"
-		var defaultBoxShadow = "4px 4px 3px 1px rgba(29, 30, 31, 0.25)"
+	} else if (cookies["night-mode"] === 'false' || cookies["night-mode"] === false ){
+		userProfilePhotoBoxShadow = "0px 0px 35px 1px rgba(29, 30, 31, 0.87)"
+		mainPageBackgroundColor = "#D1D1D1"
+		buttonBackGroundColor = "#DFDFDF"
+		navBarBackGroundColor = "#EDEDED"
+		iconsColor = "#000000"
+		fontColor = "#000000"
+		innerImageBlockProjectPage = "#AFAFAF"
+		defaultBoxShadow = "4px 4px 3px 1px rgba(29, 30, 31, 0.25)"
 		
 		var newNightMode = {
 			iconsColor,
@@ -59,14 +59,14 @@ export default function SetVariables() {
 	} else {
 		setCookies('night-mode', false, { path:'/' })
 
-		var userProfilePhotoBoxShadow = "0px 0px 35px 1px rgba(29, 30, 31, 0.87)"
-		var mainPageBackgroundColor = "#D1D1D1"
-		var buttonBackGroundColor = "#DFDFDF"
-		var navBarBackGroundColor = "#EDEDED"
-		var iconsColor = "#000000"
-		var fontColor = "#000000"
-		var innerImageBlockProjectPage = "#AFAFAF"
-		var defaultBoxShadow = "4px 4px 3px 1px rgba(29, 30, 31, 0.25)"
+		userProfilePhotoBoxShadow = "0px 0px 35px 1px rgba(29, 30, 31, 0.87)"
+		mainPageBackgroundColor = "#D1D1D1"
+		buttonBackGroundColor = "#DFDFDF"
+		navBarBackGroundColor = "#EDEDED"
+		iconsColor = "#000000"
+		fontColor = "#000000"
+		innerImageBlockProjectPage = "#AFAFAF"
+		defaultBoxShadow = "4px 4px 3px 1px rgba(29, 30, 31, 0.25)"
 		
 		var newNightMode = {
 			iconsColor,
